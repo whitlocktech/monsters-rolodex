08b83201f6dd46752a07d21dda2731e88fa01164
whitlocktech <whitlocktech@gmail.com>
2023-05-17 11:10:25 -0500

    Lessons complete for day

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import Cardlist from './components/card-list/card-list.componet';
 
 import './App.css';
 
@@ -7,37 +8,53 @@ class App extends Component {
     super()
     
     this.state = {
-      monsters: [
-        {
-          name: 'Linda',
-          id: '1'
-        },
-        {
-          name: 'Frank',
-          id: '2'
-        },
-        {
-          name: 'Jacky',
-          id: '3'
-        },
-        {
-          name: 'Andrei',
-          id: '4'
-        }
-      ]
+      monsters: [],
+      searchField: '',
     }
+    console.log('constructor')
+  }
+  
+  componentDidMount() {
+    console.log('componentDidMount')
+    fetch('https://jsonplaceholder.typicode.com/users') // fetch is a method that returns a promise
+      .then(response => response.json()) // response.json() is a method that returns a promise
+      .then((users) => this.setState(() => {
+        return { monsters: users }
+      }))
   }
 
+  onSearchChange = (event) => {
+        const searchField = event.target.value.toLowerCase()
+    this.setState(() => {
+      return { searchField }
+    })}
+  
   render() {
-    return <div className="App">
-      {
-        this.state.monsters.map((monster) => {
-          return <div key={monster.id}>
-            <h1>{monster.name}</h1>
-          </div>
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this
+    console.log('render')
+    
+        const filteredMonsters = monsters.filter((monster) => {
+          return monster.name.toLowerCase().includes(searchField)
         })
+    
+    return <div className="App">
+      <input className='search-Box'
+        type='search'
+        placeholder='search monsters'
+        onChange={onSearchChange} />
+      
+      {
+        // {
+        //   filteredMonsters.map((monster) => {
+        //     return <div key={monster.id}>
+        //       <h1>{monster.name}</h1>
+        //     </div>
+        //   })
+        // }
       }
-    </div>
+        <Cardlist />
+        </div>
   }
 }
 
