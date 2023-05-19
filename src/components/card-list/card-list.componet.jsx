a617fbf1bb451375f3120eb94496c2285a017b82
whitlocktech <whitlocktech@gmail.com>
2023-05-18 23:08:56 -0500

    Changed to React 18

--- src/components/card-list/card-list.componet.jsx
+++ src/components/card-list/card-list.componet.jsx
@@ -6,7 +6,7 @@ import './card-list.styles.css'
 import Card from '../card/card.component'
 
 const CardList = ({ monsters }) => ( 
-    <div className="card-list">
+    <div className="card-list" >
         {monsters.map((monster) => {
             return (
                 <Card monster={monster} />
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// React 17
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// React 18
+const container = document.getElementById('root');
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
