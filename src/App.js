42a4f725a083f24a72753b6d58e135d3c1326ef3
whitlocktech <whitlocktech@gmail.com>
2023-05-18 22:20:14 -0500

    switched the monsters to cats

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
-import { Component } from 'react';
+//import { Component } from 'react';
 import Cardlist from './components/card-list/card-list.componet';
 import SearchBox from './components/search-box/search-box.component'
+import {useState, useEffect} from 'react'
 
 import './App.css';
 
+const App = () => { 
+  console.log('render')
+  const [searchField, setSearchField] = useState('') // [values, setValues] is how this works
+  console.log(searchField)
+  const [monsters, setMonsters] = useState([])
+  const[filteredMonsters, setFilteredMonsters] = useState(monsters)
+  
+  useEffect(() => {
+    console.log('useEffect')
+    fetch('https://jsonplaceholder.typicode.com/users') // fetch is a method that returns a promise
+    .then(response => response.json()) 
+    .then((users) => setMonsters(users))
+  }, []) // empty array means only run once
+  useEffect(() => { 
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField)
+    })
+
+    setFilteredMonsters(newFilteredMonsters)
+  }, [monsters, searchField])
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLowerCase()
+    setSearchField(searchFieldString)
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField)
+    })
+    console.log(filteredMonsters)
+  }
+
+  return (
+    <div className="App">
+      <h1 className='app-title'>Kitty Rolodex</h1>
+        <SearchBox 
+          className='search-box'
+          placeholder='search kitties'
+        onChangeHandler={onSearchChange}
+      />
+        <Cardlist monsters={filteredMonsters} />
+      {
+        /*
+      */
+      }
+      </div>
+  )
+
+}
+
+/*
 class App extends Component {
   constructor() {
     super()
@@ -36,13 +87,7 @@ class App extends Component {
           return monster.name.toLowerCase().includes(searchField)
         })
     
-    return <div className="App">
-      <h1 className='app-title'>Kitty Rolodex</h1>
-      <SearchBox onChangeHandler={onSearchChange} placeholder='Search Cats'
-        className='monsters-search-box' />
-      <Cardlist monsters={filteredMonsters} />
-        </div>
-  }
+    return ()
 }
-
+*/
 export default App;
